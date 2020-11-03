7fa42e601b72c22cbb52e0fcdec8e8610abd90fb
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-03 07:24:39 -0500

    seeds.sql addition

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
