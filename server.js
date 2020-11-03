2bf23110a4c72540438cdb37395e3c94fbea4d8b
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-03 12:14:47 -0500

    completion of server.js code

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controllers");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.engine("handlebars", exphbs({ defaultLayout: main }));
+app.set("view enginer", "handlebars");
+
+app.listen(PORT, function(err) {
+    if (err) {
+        console.error("Error connecting " + err.stack);
+    }
+    console.log(`App listening on ${PORT}`);
+})
