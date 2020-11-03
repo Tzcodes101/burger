d82c6a53ab7269abe8942fe896354185f92a624b
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-03 10:23:32 -0500

    get and post routes added

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+
+//create router
+const router = express.Router();
+
+//get route for index (aka home)
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+});
+
+//get route for all
+router.get("/burgers", function(req, res) {
+    burger.selectAll(function(burgerData) {
+        res.render("index", {burger_date: burgerData})
+    });
+});
+
+//post route, then go back to home page 
+router.post("/burgers/add", function(req, res) {
+    burger.insertBurger(req.body.burger_name, function(result){
+        console.log("Your new burger: " + result);
+        res.redirect("/");
+    })
+})
+
+//put route
+
+
+
+
+//export router
