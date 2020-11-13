d00cb94ddbd5fe9c5fa3c315990ada06d9dc8918
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-11-13 10:25:59 -0500

    add port back in

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require("./controllers/burgers_controllers.js");
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
